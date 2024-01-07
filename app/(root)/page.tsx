7e99eb2df9570df651e33c141cccd1fc06837094
sureshYanamula <ysureshy.1994@gmail.com>
2024-01-07 06:00:06 +0530

    started working on event organizing app

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <Button variant="destructive" className="px-10">
+        Destructive
+      </Button>
+    </main>
+  );
+}
