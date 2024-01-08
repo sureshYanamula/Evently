3479eca783f047fd4eebfaf7ee707995de29d344
sureshYanamula <ysureshy.1994@gmail.com>
2024-01-08 06:46:53 +0530

    Added header and footer to the home page and also signup functioaliity using clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <main>
-      <Button variant="destructive" className="px-10">
-        Destructive
-      </Button>
-    </main>
-  );
+  return <main></main>;
 }
